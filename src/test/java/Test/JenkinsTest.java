c363d0f36ad3dc9614758dfe2adf8c88554295a7
Karol Marczyk <KarMarcz@gmail.com>
2018-11-03 18:53:50 +0100

    Adding Remote Browser

--- src/test/java/Test/JenkinsTest.java
+++ src/test/java/Test/JenkinsTest.java
@@ -6,14 +6,19 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class JenkinsTest {
 
-
+    private WebDriver driver;
 
 
 
@@ -27,9 +32,12 @@ public class JenkinsTest {
 
 
     @Test
-    public void testGutu99(){
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
-        WebDriver driver = new ChromeDriver();
+    public void testGutu99() throws MalformedURLException {
+//        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+//        driver = new ChromeDriver();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+        driver = new RemoteWebDriver(new URL("http://chrome:4444/wd/hub/"), capabilities);
         driver.get("http://demo.guru99.com/test/guru99home/");
         WebDriverWait wait = new WebDriverWait(driver, 10);
         WebElement webElement = driver.findElement(By.xpath("//div[@id='site-name']"));
